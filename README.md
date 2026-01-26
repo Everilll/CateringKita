c11ed8cc8291b436c243b7b4c732c7857522d3d1
Everilll <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2026-01-26 07:41:16 +0700

    Initial Commit: Setup NestJS

---
+++ README.md
@@ -0,0 +1,19 @@
+```
+# CateringKita
+
+Platform pemesanan catering online
+
+## Struktur Project
+
+- backend/ - NestJS API
+- frontend/ - Coming soon
+
+## Setup Backend
+```
+cd backend
+npm install
+npm run start:dev
+```
+
+## Author
+Everill - Kelas 11
---
+++ backend/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { BcryptModule } from './bcrypt/bcrypt.module';
+import { MenusModule } from './menus/menus.module';
+import { CustomersModule } from './customers/customers.module';
+import { VendorsModule } from './vendors/vendors.module';
+import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [PrismaModule, BcryptModule, MenusModule, CustomersModule, VendorsModule, CategoriesModule, OrdersModule, PaymentsModule, ReviewsModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
